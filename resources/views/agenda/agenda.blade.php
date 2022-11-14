7f916a125e83f69b754f8ca7f7b624952b66e465
najatrirazaqa <trirazaqa@gmail.com>
2022-11-14 11:21:53 +0700

    update table agenda

--- resources/views/agenda/agenda.blade.php
+++ resources/views/agenda/agenda.blade.php
@@ -1,50 +1,112 @@
 @include('layouts.header')
 @include('layouts.navbar')
 @include('layouts.sidebar')
-@include('layouts.footer')
-<!DOCTYPE html>
-<html>
+  <!-- DataTables -->
+  <link rel="stylesheet" href="{{asset('AdminLTE')}}/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="{{asset('AdminLTE')}}/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="{{asset('AdminLTE')}}/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
 
-<head>
-  <title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6">
+              <h1>Agenda</h1>
+            </div>
+            <div class="col-sm-6">
+              <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item active">Agenda</li>
+              </ol>
+            </div>
+          </div>
+        </div><!-- /.container-fluid -->
+      </section>
 
-<body>
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col">
+                <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title"> Agenda Hari Ini</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                  <table id="example1" class="table table-bordered table-striped">
+                  <a href="/agenda/tambah"><button type="button" class="btn btn-primary mb-2"> + Tambah Data Baru</button></a>
 
-  <a href="/agenda/tambah"> + Tambah Pegawai Baru</a>
-
-
-  <div class="card">
-    <div class="card-header">
-      <h3 class="card-title">DataTable with default features</h3>
+                    
+                    <thead>
+                      <tr>
+                        <th>No</th>
+                        <th>Nama Kegiatan</th>
+                        <th>Implementasi Kurikulum</th>
+                        <th>Action</th>
+                      </tr>
+                    </thead>
+                    @foreach($tb_agenda as $p)
+                    <tbody>
+                      <tr>
+                          <td>{{ $p->id }}</td>
+                          <td>{{ $p->nama_kegiatan }}</td>
+                          <td>{{ $p->implementasi_kurikulum }}
+                          </td>
+                          <td>
+        <a href="/agenda/edit/{{ $p->id }}">Edit</a>
+        |
+        <a href="/agenda/hapus/{{ $p->id }}">Hapus</a>
+      </td>
+                      </tr>
+                    
+                    </tbody>
+                    @endforeach
+                  </table>
+                </div>
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
     </div>
-    <!-- /.card-header -->
-    <div class="card-body">
-      <table id="example1" class="table table-bordered table-striped">
-        <thead>
-          <tr>
-            <th>Nama Kegiatan</th>
-            <th>Implementasi Kurikulum</th>
-          </tr>
-        </thead>
-        @foreach($tb_agenda as $p)
-        <tbody>
-          <tr>
-            <td>{{ $p->nama_kegiatan }}</td>
-            <td>{{ $p->implementasi_kurikulum }}</td>
-            <td>
-              <a href="/agenda/edit/{{ $p->id }}">Edit</a>
-              |
-              <a href="/agenda/hapus/{{ $p->id }}">Hapus</a>
-            </td>
-          </tr>
-          @endforeach
-      </table>
-    </div>
-    <!-- /.card-body -->
-  </div>
+    @include('layouts.footer')
+      <!-- DataTables  & Plugins -->
+  <script src="{{asset('AdminLTE')}}/plugins/datatables/jquery.dataTables.min.js"></script>
+  <script src="{{asset('AdminLTE')}}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <script src="{{asset('AdminLTE')}}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+  <script src="{{asset('AdminLTE')}}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+  <script src="{{asset('AdminLTE')}}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+  <script src="{{asset('AdminLTE')}}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+  <script src="{{asset('AdminLTE')}}/plugins/jszip/jszip.min.js"></script>
+  <script src="{{asset('AdminLTE')}}/plugins/pdfmake/pdfmake.min.js"></script>
+  <script src="{{asset('AdminLTE')}}/plugins/pdfmake/vfs_fonts.js"></script>
+  <script src="{{asset('AdminLTE')}}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+  <script src="{{asset('AdminLTE')}}/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+  <script src="{{asset('AdminLTE')}}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 
-
-</body>
-
-</html>
+  <script>
+    $(function () {
+      $("#example1").DataTable({
+        "responsive": true, "lengthChange": false, "autoWidth": false,
+        "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+      }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+      $('#example2').DataTable({
+        "paging": true,
+        "lengthChange": false,
+        "searching": false,
+        "ordering": true,
+        "info": true,
+        "autoWidth": false,
+        "responsive": true,
+      });
+    });
+  </script>
--- resources/views/agenda/tambah.blade.php
+++ resources/views/agenda/tambah.blade.php
@@ -1,33 +1,46 @@
 @include('layouts.header')
 @include('layouts.navbar')
 @include('layouts.sidebar')
-@include('layouts.footer')
-<!DOCTYPE html>
-<html>
+<!-- Main content -->
+<section class="content">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+                <div class="card">
+                <div class="card-header">
+                  <h3 class="card-title"> Agenda Hari Ini</h3>
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                  <table id="example1" class="table table-bordered table-striped">
+                  <form action="/agenda/store" method="post">
+                  {{ csrf_field() }}
+                  <a href="/agenda"> Kembali</a>
+                  <tbody>
+                  <tr>
+                    <td> Nama Kegiatan<input type="text" name="nama_kegiatan"></td>
+                  </tr>
+                  <tr>
+                    <td> Implementasi Kurikulum<input type="text" name="implementasi_kurikulum"></td>
+                  </tr>
+                  <tr>
+                    <td><input type="submit" value="Simpan Data"></td>
+                  </tr>
+                    </tbody>
+                    </form>
+                  </table>
+                </div>
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
 
-<head>
-  <title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
 
-<body>
-
-  <h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-  <h3>Data Pegawai</h3>
-
-  <a href="/agenda"> Kembali</a>
-
-  <br />
-  <br />
-
-  <form action="/agenda/store" method="post">
-    {{ csrf_field() }}
-    Nama Kegiatan<input type="text" name="nama_kegiatan"> <br />
-    Implementasi Kurikulum<input type="text" name="implementasi_kurikulum"> <br />
-    <input type="submit" value="Simpan Data">
-  </form>
-
-
-
-</body>
-
-</html>
+  @include('layouts.footer')
